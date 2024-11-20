33035a53bc27418bde00ecb7fd5005f4a488b171
Khang0609 <trannguyenkhang836@gmail.com>
2024-11-20 18:07:20 +0700

    BCCK

--- portfolio-github/in4/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class In4Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'in4'
--- portfolio-github/in4/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import info
-
-# Create your views here.
-
-def list(request):
-    Data = {'Infomation': info.objects.all().order_by('-id')}
-    return render(request, 'in4\in4.html', Data)
